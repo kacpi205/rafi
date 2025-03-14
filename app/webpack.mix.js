62ba57629dd35ed15faae221238c1385d449d13b
kacpi205 <miszczuc1@gmail.com>
2025-03-14 08:46:00 +0100

    Delete app directory

--- app/webpack.mix.js
+++
@@ -1,9 +0,0 @@
-let mix = require("laravel-mix")
-let tailwindcss = require("tailwindcss")
-let path = require("path")
-
-
-mix
-	.setPublicPath("public")
-	.js("resources/js/field.js", "js")
-	.postCss("resources/css/field.css", "css", [tailwindcss("tailwind.config.js")])
